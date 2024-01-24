bafd7a64d32ca6b589bda04fb4d1132fe166896e
miguelbenavidez432 <119986047+miguelbenavidez432@users.noreply.github.com>
2024-01-23 21:36:25 -0300

    fixed

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -38,14 +38,14 @@ function About() {
                 </div> */}
                 <div className={`${styles.flexCenter} ${styles.paddingX} py-4 mt-8 container `} >
 
-                    <img src={agencia_2} alt="" className='me-96 p-0 absolute pl-12 ' />
+                    <img src={agencia_2} alt="" className='me-60 p-0 absolute pl-12' />
 
                     <img src={agencia_horizontal} alt="" className='absolute mx-8 my-14 border-slate-500 border-2 invisible md:visible' />
                     {/* <h1 className='text-black flex-1 font-poppins font-semibold 
                 ss:text[72px] text-[52px] ss:leading-[100px] leading-[75px] mr-2 '>
                         AGENCIA
                     </h1> */}
-                    <p className={`${styles.paragraph} max-w-[770px] mt-5 md:text[22px] sm:relative text-right text-white ms-60 pr-10`}>
+                    <p className={`${styles.paragraph} max-w-[770px] mt-5 md:text[22px] sm:relative text-left text-white ms-60 pr-20`}>
                         En nuestra agencia de publicidad nos enorgullece ofrecer soluciones creativas e innovadoras para promover y
                         posicionar marcas en el mercado. Nuestro equipo de expertos multidisciplinarios está comprometido en ofrecer
                         resultados excepcionales a nuestros clientes al implementar estrategias de marketing efectivas y campañas
@@ -56,10 +56,10 @@ function About() {
                 </div>
                 <div className={`${styles.flexCenter} ${styles.paddingX} ${styles.paddingY} ${styles.marginX} mt-16`}>
 
-                    <img src={central_de_medios_2} alt="" className='ms-72 p-0 absolute max-w-screen-sm size-60' />
+                    <img src={central_de_medios_2} alt="" className='ms-56 p-0 absolute max-w-screen-sm size-60' />
 
                     <img src={central_de_medios_horizontal} alt="" className='absolute mx-8 my-14 border-slate-500 border-2 invisible md:visible' />
-                    <p className={`${styles.paragraph} max-w-[770px] relative text-white md:text-start me-60 text-center `}>
+                    <p className={`${styles.paragraph} max-w-[770px] sm:relative text-white md:text-start me-60 text-right pl-10 `}>
                         En nuestra central de medios nos dedicamos a ofrecer soluciones integrales y efectivas para la planificación,
                         compra y ejecución de estrategias de medios. Nuestro objetivo es ayudar a las marcas a maximizar su alcance e
                         impacto en el mercado, optimizando cada dólar invertido en publicidad.
@@ -72,9 +72,9 @@ function About() {
                     </h1> */}
                 </div>
                 <div className={`${styles.paddingX} ${styles.flexCenter} my-8 mx-8 py-16 px-16`}>
-                    <img src={consultoria_2} alt="" className='me-72 p-0 absolute max-w-screen-sm size-60' />
+                    <img src={consultoria_2} alt="" className='me-48 p-0 absolute size-52' />
                     <img src={consultoria_horizontal} alt="" className='absolute mx-8 my-14 border-slate-500 border-2 invisible md:visible' />
-                    <p className={`${styles.paragraph} max-w-[770px] mt-5 ss:text[22px] md:relative text-right text-white ms-60 `}>
+                    <p className={`${styles.paragraph} max-w-[770px] mt-5 ss:text[22px] md:relative text-left text-white ms-60 pr-16 md:text-end pl-10`}>
                         En nuestra consultoría de comunicación y estrategia nos especializamos en ayudar a las empresas a
                         desarrollar y ejecutar estrategias de comunicación efectivas y a medida. Nuestro enfoque radica en comprender
                         a fondo las necesidades y objetivos de cada cliente, para así poder diseñar soluciones personalizadas que generen
